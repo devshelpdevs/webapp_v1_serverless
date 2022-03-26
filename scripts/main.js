af6c9bcfb4fc4e1118bdb07421a7e9c42f4d0804
biggaji <herityjohnny14@gmail.com>
2022-03-26 13:25:30 +0100

    completed the index page structure

--- scripts/main.js
+++ scripts/main.js
@@ -3,3 +3,20 @@
 const currentYear = new Date().getFullYear();
 const yearDisplay = document.getElementById("footer_year");
 yearDisplay.innerHTML = currentYear;
+
+// faq functionality
+let acc = document.getElementsByClassName("accordion");
+let i;
+
+// loop throught the accordions
+for(i = 0; i < acc.length; i++) {
+    acc[i].addEventListener("click", function() {
+        this.classList.toggle("active");
+        let panel = this.nextElementSibling;
+        if(panel.style.maxHeight) {
+            panel.style.maxHeight = null;
+        } else {
+            panel.style.maxHeight = panel.scrollHeight + "px";
+        };
+    });
+};
